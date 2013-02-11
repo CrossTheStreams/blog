41bc97a0a020ad63190334b23a070224ccec77bd
Andrew <arhautau@gmail.com>
2013-02-10 18:38:04 -0800

    assets aaaaah

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//
-
+//= require bootstrap-transition
+//= require bootstrap-dropdown
+//= require bootstrap-typeahead
+//=
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
