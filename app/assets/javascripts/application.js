b52568d1f067cb08764a5e38e30ed4c3e60f8be9
Andrew <arhautau@gmail.com>
2013-02-10 17:29:17 -0800

    hopefully this works?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require underscore.v142.min
-//= require backbone.v092.min
-//= require bootstrap-dropdown
-//= require bootstrap-transition
-//= require bootstrap-typeahead
-//= require blog
 
 //= require_tree ../templates
 //= require_tree ./models
